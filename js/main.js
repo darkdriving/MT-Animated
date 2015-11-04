009e455fe63b31295ed83a17642a38eb21ad473d
Andrew Gormley <agormley@monetate.com>
2015-11-04 15:11:05 -0500

    Bunch of stuff added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
+$(function() {
 
+    $('#removeClasses').click(function(){
+        $('#mt-leftRail').toggleClass('mt-reverse');
+    });
+
+});
+
+
+
+/* From Modernizr */
+function whichTransitionEvent(){
+    var t;
+    var el = document.getElementById('mt-inlineBanner');
+    var transitions = {
+      'transition':'transitionend',
+      'OTransition':'oTransitionEnd',
+      'MozTransition':'transitionend',
+      'WebkitTransition':'webkitTransitionEnd'
+    }
+
+    for(t in transitions){
+        if( el.style[t] !== undefined ){
+            return transitions[t];
+        }
+    }
+}
+
+/* Listen for a transition! */
+var transitionEvent = whichTransitionEvent();
+transitionEvent && e.addEventListener(transitionEvent, function() {
+	alert('Transition complete!  This is the callback, no library needed!');
+});
+
+/*
+	The "whichTransitionEvent" can be swapped for "animation" instead of "transition" texts, as can the usage :)
+*/
